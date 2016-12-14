211d1a62eb8240529d997d1425f1d34a74c2341d
Erika Shewan <eshewan@ssmic.com>
2016-12-14 10:45:27 -0500

    initial commit

---
+++ boilerplate-mvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(boilerplate_mvc.Startup))]
+namespace boilerplate_mvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
